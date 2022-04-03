81afca72eac8bb5b66bc0a688abc9a5b993f4a9b
jiltq <89758063+jiltq@users.noreply.github.com>
2022-04-03 18:22:55 -0500

    Add files via upload

---
+++ precache-manifest.ade784b6c8eed2fdeed0d47831577dcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1993e6a885c4ce119e672575288894bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23bb3a6fb6eefac7f713",
+    "url": "/static/css/main.75f62805.chunk.css"
+  },
+  {
+    "revision": "df31fe1e5b38fb5e2a85",
+    "url": "/static/js/2.2d788cd3.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/static/js/2.2d788cd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23bb3a6fb6eefac7f713",
+    "url": "/static/js/main.9964d88a.chunk.js"
+  },
+  {
+    "revision": "fc234bd707874dbe3e7a",
+    "url": "/static/js/runtime-main.07cf8cf7.js"
+  }
+]);
