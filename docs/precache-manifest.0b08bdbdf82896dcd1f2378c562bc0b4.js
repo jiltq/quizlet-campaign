fa81d6518f2c5f2166da77b7d8460a660f2e18d1
jiltq <89758063+jiltq@users.noreply.github.com>
2022-04-06 21:18:57 -0500

    Add files via upload

---
+++ docs/precache-manifest.0b08bdbdf82896dcd1f2378c562bc0b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f253a14636556f5131530a5ed6bca0",
+    "url": "/quizlet-campaign/index.html"
+  },
+  {
+    "revision": "a181ae7572e0b13192f5",
+    "url": "/quizlet-campaign/static/css/main.a12ff4a9.chunk.css"
+  },
+  {
+    "revision": "0906d3c31a17dfe71bfa",
+    "url": "/quizlet-campaign/static/js/2.cf136e1b.chunk.js"
+  },
+  {
+    "revision": "5843ec24999d9bc4d5892cbe88e32df5",
+    "url": "/quizlet-campaign/static/js/2.cf136e1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a181ae7572e0b13192f5",
+    "url": "/quizlet-campaign/static/js/main.8c3c18db.chunk.js"
+  },
+  {
+    "revision": "494217c5fdeac4b8fcc3",
+    "url": "/quizlet-campaign/static/js/runtime-main.5b14adce.js"
+  }
+]);
